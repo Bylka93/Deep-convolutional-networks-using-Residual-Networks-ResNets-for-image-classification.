5d17275c88571fc31174d3877579661c1dc9bfd8
Bylka93 <44440592+Bylka93@users.noreply.github.com>
2020-01-24 10:12:05 +0200

    Update res_net.py

--- res_net.py
+++ res_net.py
@@ -68,7 +68,7 @@ def identity_block(X, f, filters, stage, block):
     return X
 
 # convolution block
-# GRADED FUNCTION: convolutional_block
+
 
 
 def convolutional_block(X, f, filters, stage, block, s=2):
@@ -92,14 +92,14 @@ def convolutional_block(X, f, filters, stage, block, s=2):
     X = BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
     X = Activation('relu')(X)
 
-    # Second component of main path (≈3 lines)
+    # Second component of main path 
     X = Conv2D(F2, (f, f), strides=(1, 1), padding='same',
                name=conv_name_base + '2b',
                kernel_initializer=glorot_uniform(seed=0))(X)
     X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
     X = Activation('relu')(X)
 
-    # Third component of main path (≈2 lines)
+    # Third component of main path 
     X = Conv2D(F3, (1, 1), strides=(1, 1),
                padding='valid', name=conv_name_base + '2c',
                kernel_initializer=glorot_uniform(seed=0))(X)
@@ -116,7 +116,6 @@ def convolutional_block(X, f, filters, stage, block, s=2):
 
     return X
 # building the 50 layer of res_net
-# GRADED FUNCTION: ResNet50
 
 
 def ResNet50(input_shape=(64, 64, 3), classes=6):
@@ -143,14 +142,14 @@ def ResNet50(input_shape=(64, 64, 3), classes=6):
     X = identity_block(X, 3, [64, 64, 256], stage=2, block='b')
     X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
 
-    # Stage 3 (≈4 lines)
+    # Stage 3 
     X = convolutional_block(
         X, f=3, filters=[128, 128, 512], stage=3, block='a', s=2)
     X = identity_block(X, 3, [128, 128, 512], stage=3, block='b')
     X = identity_block(X, 3, [128, 128, 512], stage=3, block='c')
     X = identity_block(X, 3, [128, 128, 512], stage=3, block='d')
 
-    # Stage 4 (≈6 lines)
+    # Stage 4 
     X = convolutional_block(
         X, f=3, filters=[256, 256, 1024], stage=4, block='a', s=2)
     X = identity_block(X,  3, [256, 256, 1024], stage=4, block='b')
@@ -159,13 +158,13 @@ def ResNet50(input_shape=(64, 64, 3), classes=6):
     X = identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
     X = identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
 
-    # Stage 5 (≈3 lines)
+    # Stage 5 
     X = convolutional_block(
         X, f=3, filters=[512, 512, 2048], stage=5, block='a', s=2)
     X = identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
     X = identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
 
-    # AVGPOOL (≈1 line). Use "X = AveragePooling2D(...)(X)"
+    # AVGPOOL 
     X = AveragePooling2D(pool_size=(2, 2), name='avg_pool')(X)
 
     # output layer
